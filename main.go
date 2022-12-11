f76b90bc9975eca2c37209d7461cf85bcfa2786d
Umar Dafedar <108915275+UmarDafedarProgrammer@users.noreply.github.com>
2022-12-11 20:09:02 +0530

    Update main.go

--- main.go
+++ main.go
@@ -4,5 +4,11 @@ import "fmt"
 
 func main() {
 	fmt.Println("main function is called")
+	fmt.Println("-------------------------------------")
+	// To declare and use the constants
 	Const()
+
+	fmt.Println("-------------------------------------")
+	// To identify the scenarios of data overflow
+	Overflow()
 }
