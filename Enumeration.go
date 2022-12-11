dbc4b29a006bc37f9071689da4518e78f0550225
Umar Dafedar <108915275+UmarDafedarProgrammer@users.noreply.github.com>
2022-12-11 21:10:00 +0530

    Create Enumeration.go

---
+++ Enumeration.go
@@ -0,0 +1,116 @@
+package main
+
+import "fmt"
+
+/*
+	Enum is used to define a data type that has set of named related values called elements, enumerals, enumerators
+	It will have set of values, which are considered valid.
+	in Go, there is no concept called enums.instead we use const to define enums
+*/
+
+func Enum() {
+	fmt.Println("Enum function is called")
+
+	/*	const (
+				a
+				b
+				c
+			)
+
+		ERROR:
+			Missing init*/
+
+	// fmt.Println(iota)
+	// ERROR: cannot use iota outside constant declaration
+
+	const (
+		a = iota
+		b
+		c
+	)
+	fmt.Println(a, b, c) // 0 1 2
+
+	const (
+		d = iota + 1
+		e
+		f
+	)
+	fmt.Println(d, e, f) // 1 2 3
+
+	const (
+		g = 1 + iota
+		h
+		i
+	)
+	fmt.Println(g, h, i) // 1 2 3
+
+	const (
+		j = 1 + iota
+		k = 2 + iota
+		l
+	)
+	fmt.Println(j, k, l) // 1 3 4
+
+	const (
+		m = 1
+		n
+		o
+	)
+	fmt.Println(m, n, o) //1 1 1
+
+	const (
+		p = iota
+		q = iota
+		r = iota
+	)
+	fmt.Println(p, q, r) // 0 1 2
+
+	const (
+		s = 0 - iota
+		t
+		u
+	)
+	fmt.Println(s, t, u) // 0 -1 -2
+
+	const (
+		v = 10 - iota
+		_ // blank identifier to skip a value in a list of constants.
+		w
+		x
+	)
+	fmt.Println(v, w, x) // 10 8 7
+
+	// Multiple iotas in a single line
+	const (
+		q1, q2, q3 = iota, iota, iota
+		r1, r2, r3
+		s1, s2, s3
+	)
+
+	fmt.Printf("q1: %d, q2: %d, q3: %d r1: %d, r2: %d, r3: %d s1: %d, s2: %d, s3: %d", q1, q2, q3, r1, r2, r3, s1, s2, s3)
+
+	// multi-line strings - `` quotes
+	fmt.Printf(`
+	q1: %d, q2: %d, q3: %d 
+	r1: %d, r2: %d, r3: %d 
+	s1: %d, s2: %d, s3: %d`, q1, q2, q3, r1, r2, r3, s1, s2, s3)
+
+	const (
+		Sunday int = iota
+		Monday
+		Tuesday
+		Wednesday
+		Thursday
+		Friday
+		Saturday
+	)
+	fmt.Printf(`
+	Sunday		:%d
+	Monday		:%d
+	Tuesday		:%d
+	Wednesday	:%d
+	Thursday	:%d
+	Friday		:%d
+	Saturday	:%d`, Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday)
+
+}
